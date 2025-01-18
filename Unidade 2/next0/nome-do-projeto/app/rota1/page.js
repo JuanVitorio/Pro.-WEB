5343a4652cea56754457331092bf5343fd4a23e5
Juan Vitório Dutra de Araújo <88391640+JuanVitorio@users.noreply.github.com>
2025-01-17 21:40:10 -0300

    criação de projeto react

---
+++ Unidade 2/next0/nome-do-projeto/app/rota1/page.js
@@ -0,0 +1,26 @@
+export default function Home() {
+    return (
+      <main>
+        <h1>React é muito é massa</h1>
+        <p>página</p>
+      </main>
+    );
+  }
+
+  export function About() {
+    return (
+      <section>
+        <h2>Eu</h2>
+        <p>Esta é a seção sobre.</p>
+      </section>
+    );
+  }
+  
+  export function Contact() {
+    return (
+      <section>
+        <h2>Contato</h2>
+        <p>não ligue pra mim</p>
+      </section>
+    );
+  }
